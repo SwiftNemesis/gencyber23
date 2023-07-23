d6680fb78ec2f1a40c6a5127fe9e7bae8407c389
SwiftNemesis <bbsowers@gmail.com>
2023-07-23 12:23:41 -0700

    Intial commit

---
+++ installScript.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Install docker and docker compose
+
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker --now 
+sudo usermod -aG docker kali
+
+printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+#Install Steghide
+sudo apt install -y steghide
+
+#Install gdown for downloading files from google drive
+cd ~/Downloads
+pip3 install gdown
+
+#Download Background photo
+gdown https://drive.google.com/uc?id=1G3ocEuBSHqVejQS_P8lAvXlUNhUnMY-S
+
+#Set Background photo for XFCE desktop
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor1/workspace0/last-image -s ~/Downloads/ace.jpg
+rm ~/Downloads/ace.jpg
+
+#Placehodler for Bianca's WP lab
+
+
+#Install Coltons Cyber Clinic lab
+cd ~
+git clone https://github.com/colton-gabertan/cyber-clinic-lab.git
+cd cyber-clinic-lab
+#This may take up to 10 minutes
+echo "Spinning up containers for the lab, this may take up to **10 minutes**"
+docker compose up -d
+docker compose down -v
