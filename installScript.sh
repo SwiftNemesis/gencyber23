021b5518c3f487686c396fe6714f40f5888cfe40
SwiftNemesis <bbsowers@gmail.com>
2023-07-23 15:18:23 -0700

    upated

--- installScript.sh
+++ installScript.sh
@@ -58,11 +58,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-#Docker Compose Install
-cd ~/Downloads
-curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-sudo chmod +x docker-compose-linux-x86_64
-sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER
 newgrp docker
