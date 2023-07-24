7e725fec6f4773b0aa227fce9070e6acf1b9e141
SwiftNemesis <bbsowers@gmail.com>
2023-07-23 17:30:54 -0700

    Updated script# Please enter the commit message for your changes. Lines starting

--- installScript.sh
+++ installScript.sh
@@ -6,12 +6,13 @@ sudo apt update
 
 printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+y
 
 sudo apt update
 sudo apt install -y docker.io
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-# sudo systemctl enable docker --now 
-# sudo apt install -y docker.io
+sudo systemctl enable docker --now 
+sudo apt install -y docker.io
 
 sudo usermod -aG docker kali
 
